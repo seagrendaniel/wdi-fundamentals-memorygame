92555e8b05b3dda600316b9c83a73a84cf1eae48
blackdanamite <seagrendaniel@gmail.com>
2019-06-20 15:06:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,21 +3,26 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay[0]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+		}
+		else {
+		console.log("Sorry, try again.");
+		}
 	}
 }
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(1);
+
+
+
+
+
