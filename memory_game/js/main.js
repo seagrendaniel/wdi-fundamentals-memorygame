e9e9c24486ffd0040bb608d15e9d9089e24a0d19
blackdanamite <seagrendaniel@gmail.com>
2019-06-20 20:01:18 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,49 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if(cardsInPlay.length === 2) {
 		if(cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 		}
 		else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		}
 	}
 }
 
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank + " of " + cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 	cardsInPlay.push(cards[cardID]);
 	checkForMatch();
 }
 flipCard(0);
-flipCard(1);
+flipCard(0);
 
 
 
