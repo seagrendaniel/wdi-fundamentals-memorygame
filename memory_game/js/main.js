2c1e625c62717c3250b33f03e5d12b8a5b8563dd
blackdanamite <seagrendaniel@gmail.com>
2019-06-24 14:50:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,9 +25,10 @@ var cards = [
 
 var cardsInPlay = [];
 
+
 function checkForMatch() {
 	if(cardsInPlay.length === 2) {
-		if(cardsInPlay[0] === cardsInPlay[1]) {
+		if(cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		alert("You found a match!");
 		}
 		else {
@@ -36,14 +37,26 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID].rank + " of " + cards[cardID].suit);
+function flipCard() {
+	var cardID = this.getAttribute('data-id');
 	console.log(cards[cardID].cardImage);
 	cardsInPlay.push(cards[cardID]);
+	this.setAttribute('src', cards[cardID].cardImage);
 	checkForMatch();
 }
-flipCard(0);
-flipCard(0);
+
+var createBoard = function() {
+	for(i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
 
 
 
